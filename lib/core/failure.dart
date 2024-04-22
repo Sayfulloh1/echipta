667dac5a26556009741e5c25d366d6377647a1ce
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-04-22 16:59:52 +0500

    feat: repository

---
+++ lib/core/failure.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure({
+    required this.message,
+    this.statusCode,
+  });
+
+  final String message;
+  final int? statusCode;
+
+  @override
+  List<Object?> get props => [message, statusCode];
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure({
+    required super.message,
+    super.statusCode,
+  });
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class NoInternetFailure extends Failure {
+  const NoInternetFailure({
+     super.message='No internet',
+    super.statusCode,
+  });
+
+  @override
+  List<Object?> get props => [message, statusCode];
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure({ super.message = 'No cached data found', super.statusCode});
+
+  @override
+  List<Object?> get props => [message, statusCode];
+}
--- lib/repository/auth_repo.dart
+++ lib/repository/auth_repo.dart
@@ -1,16 +1,35 @@
-class AuthRepository{
+import 'package:dio/dio.dart';
 
+import '../core/either.dart';
+import '../core/failure.dart';
+import '../core/network_info.dart';
+import '../model/match.dart';
+
+class AuthRepository {
   final Dio dio;
   final NetworkInfo networkInfo;
 
-  AuthRepository(this.dio);
+  AuthRepository(this.networkInfo, this.dio);
 
-  Future<Either<Failure,String>> sendOtp()async{
-    if(await networkInfo){
-
+  Future<Either<Failure, Match>> sendOtp() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final response = await dio.post('');
+        final model = Match.fromJson(response.data);
+        return Right(model);
+      } catch (e) {
+        if (e is DioException) {
+          return Left(
+            ServerFailure(
+              message: '${e.message}',
+              statusCode: e.response?.statusCode ?? 0,
+            ),
+          );
+        }
+        return const Left(ServerFailure(message: 'Something went wrong'));
+      }
+    } else {
+      return const Left(NoInternetFailure());
     }
-
-
-
   }
 }
